8fa1c499673fdaccbded492c2e73bbd5037c95e2
iballa003 <iballa.montero@gmail.com>
2024-10-17 13:55:12 +0100

    Mostrando título e imagen en tkinter.

--- views/view.py
+++ views/view.py
@@ -1,25 +1,34 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+
+from PIL import ImageTk,Image
+
 import main as productObjects
-import requests
-from dataclass_wizard import fromdict
+import urllib.request
+import io
 
-from models.api_response import APIResponse
+def ImgFromUrl(url):
+    with urllib.request.urlopen(url) as connection:
+        raw_data = connection.read()
+    im = Image.open(io.BytesIO(raw_data))
+    image = ImageTk.PhotoImage(im)
+    return image
 
 
 
-def mostrarLista():
-    # URL = "https://dummyjson.com/products"
-    # response = requests.get(URL)
-    # data = response.json()
-    # productList = fromdict(APIResponse, data)
-    for product in productObjects.productList.products:
-        ttk.Label(root, text=product.title).pack()
+    #-----------------------------------------------------------------------------
+    # for product in productObjects.productList.products:
+    #     ttk.Label(root, text=product.title).pack()
 
 
 root = tk.Tk()
 root.geometry("512x700")
 root.resizable(False,False)
-mostrarLista()
+image = ImgFromUrl(productObjects.productList.products[0].thumbnail)
+label1 = tk.Label(root, text=productObjects.productList.products[0].title, font='Helvetica 18 bold').pack()
+label2 = tk.Label(root, image=image)
+label2.pack()
+label3 = tk.Label(root, text="Descripción", font='Helvetica 15 bold').pack()
+label3 = tk.Label(root, text=productObjects.productList.products[0].description).pack()
 root.mainloop()
