97c106bcf2497fd5bd63e5f9a273d2a49c51a64f
iballa003 <iballa.montero@gmail.com>
2024-10-18 09:50:20 +0100

    Creado los botones siguiente, anterior y los labels precio.

--- views/view.py
+++ views/view.py
@@ -4,7 +4,7 @@ import tkinter as tk
 
 from PIL import ImageTk,Image
 
-import main as productObjects
+import main as productResponse
 import urllib.request
 import io
 
@@ -15,7 +15,10 @@ def ImgFromUrl(url):
     image = ImageTk.PhotoImage(im)
     return image
 
-
+def next():
+    print("test")
+    global productIndex
+    productIndex += 1
 
     #-----------------------------------------------------------------------------
     # for product in productObjects.productList.products:
@@ -23,12 +26,27 @@ def ImgFromUrl(url):
 
 
 root = tk.Tk()
-root.geometry("512x700")
+root.geometry("512x612")
 root.resizable(False,False)
-image = ImgFromUrl(productObjects.productList.products[0].thumbnail)
-label1 = tk.Label(root, text=productObjects.productList.products[0].title, font='Helvetica 18 bold').pack()
-label2 = tk.Label(root, image=image)
-label2.pack()
-label3 = tk.Label(root, text="Descripción", font='Helvetica 15 bold').pack()
-label3 = tk.Label(root, text=productObjects.productList.products[0].description).pack()
+productIndex = 0
+
+
+#-------------------------------------------------------------------#Title
+frame_title = Frame(root).pack()
+title_label = tk.Label(frame_title, text=productResponse.productList.products[productIndex].title, font='Helvetica 18 bold').pack()
+#-------------------------------------------------------------------#Image
+image = ImgFromUrl(productResponse.productList.products[productIndex].thumbnail)
+image_label = tk.Label(root, image=image)
+image_label.pack()
+#-------------------------------------------------------------------#Description
+frame_description = Frame(root).pack()
+descripcion_title_label = tk.Label(frame_description, text="Descripción", font='Helvetica 12 bold').pack(anchor="w")
+descripcion_content_label = tk.Label(frame_description, text=productResponse.productList.products[productIndex].description,wraplength=500, justify="left").pack()
+price_label = tk.Label(root, text="Precio: ", font='Helvetica 11 bold',pady=5).pack(anchor="w")
+price_content_label = tk.Label(root, text=productResponse.productList.products[productIndex].price).pack(anchor="w")
+#-------------------------------------------------------------------#Buttons
+button_frame = Frame(root).pack()
+previous_button = tk.Button(button_frame,text="Anterior").pack(side=LEFT)
+next_button = tk.Button(button_frame,text="Siguiente",command=next).pack(side=RIGHT)
+
 root.mainloop()
