0af34dd27d0915f1d681c01f15471890c34c976f
iballa003 <iballa.montero@gmail.com>
2024-10-23 13:54:24 +0100

    Modificado el acceso a la api y añadido el buscador.

--- views/view.py
+++ views/view.py
@@ -1,13 +1,17 @@
+from re import search
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
-
+import requests
+from dataclass_wizard import fromdict
+from models.api_response import APIResponse
 from PIL import ImageTk,Image
-
-import main as productResponse
 import urllib.request
 import io
 
+#-----------------------------------------------------------------------------#
+#                               Functions                                     #
+#-----------------------------------------------------------------------------#
 def ImgFromUrl(url):
     with urllib.request.urlopen(url) as connection:
         raw_data = connection.read()
@@ -15,48 +19,81 @@ def ImgFromUrl(url):
     image = ImageTk.PhotoImage(im)
     return image
 
+def changeContent(index):
+    title_label.config(text=productLists[index].title)
+    image2 = ImgFromUrl(productLists[index].thumbnail)
+    image_label.config(image=image2)
+    image_label.image=image2
+    descripcion_content_label.config(text=productLists[index].description)
+    price_content_label.config(text=productLists[index].price)
+    rating_content_label.config(text=productLists[productIndex].rating)
+
 def next():
-    print("test")
-    global productResponse, productIndex, title_label
-
-    title_label.config(text=productResponse.productList.products[productIndex].title)
-
+    global productIndex, productLists
     productIndex += 1
-    print(productIndex)
+    changeContent(productIndex)
 
 def previous():
-    print("test")
+    global productIndex, productLists
+    if(productIndex > 0):
+        productIndex -= 1
+        changeContent(productIndex)
 
-
-    #-----------------------------------------------------------------------------
-    # for product in productObjects.productList.products:
-    #     ttk.Label(root, text=product.title).pack()
-
-
+#-----------------------------------------------------------------------------#
+#                               API                                           #
+#-----------------------------------------------------------------------------#
+URL = "https://dummyjson.com/products"
+response = requests.get(URL)
+data = response.json()
+productsApiRequest = fromdict(APIResponse, data)
+#-----------------------------------------------------------------------------#
+#                               Tkinter                                       #
+#-----------------------------------------------------------------------------#
 root = tk.Tk()
 root.geometry("512x612")
 root.resizable(False,False)
 productIndex = 0
 
+productLists = productsApiRequest.products
 
-#-------------------------------------------------------------------#Title
+##--------##Tkinter Search bar##--------##
+search_label = ttk.Label(root, text="Buscar:", font='Helvetica 12 bold')
+search_label.pack()
+search_bar = Entry(root)
+search_bar.pack()
+Search_string = StringVar(search_bar, "")
+search_bar.config(textvariable = Search_string)
+
+##--------##Tkinter Title##--------##
 frame_title = Frame(root).pack()
-title_label = tk.Label(frame_title, text=productResponse.productList.products[productIndex].title, font='Helvetica 18 bold')
+title_label = ttk.Label(frame_title, text=productLists[productIndex].title, font='Helvetica 18 bold')
 title_label.pack()
-#-------------------------------------------------------------------#Image
-image = ImgFromUrl(productResponse.productList.products[productIndex].thumbnail)
-image_label = tk.Label(root, image=image)
+##--------##Image##--------##
+image = ImgFromUrl(productLists[productIndex].thumbnail)
+image_label = ttk.Label(root, image=image)
 image_label.pack()
-#-------------------------------------------------------------------#Description
-frame_description = Frame(root).pack()
-descripcion_title_label = tk.Label(frame_description, text="Descripción", font='Helvetica 12 bold').pack(anchor="w")
-descripcion_content_label = tk.Label(frame_description, text=productResponse.productList.products[productIndex].description,wraplength=500, justify="left").pack()
-#-------------------------------------------------------------------#Price
+##--------##Tkinter Description##--------##
+frame_description = Frame(root)
+frame_description.pack()
+descripcion_title_label = ttk.Label(frame_description, text="Descripción", font='Helvetica 12 bold')
+descripcion_title_label.pack(anchor="w")
+descripcion_content_label = ttk.Label(frame_description, text=productLists[productIndex].description,wraplength=500, justify="left")
+descripcion_content_label.pack()
+##--------##Tkinter Price##--------##
+frame_price = Frame(root).pack()
 price_label = tk.Label(root, text="Precio: ", font='Helvetica 11 bold',pady=5).pack(anchor="w")
-price_content_label = tk.Label(root, text=productResponse.productList.products[productIndex].price).pack(anchor="w")
-#-------------------------------------------------------------------#Buttons
-button_frame = Frame(root).pack()
-previous_button = tk.Button(button_frame,text="Anterior",command=previous).pack(side=LEFT)
-next_button = tk.Button(button_frame,text="Siguiente",command=next).pack(side=RIGHT)
-#-------------------------------------------------------------------#MainLoop
+price_content_label = ttk.Label(frame_price, text=productLists[productIndex].price)
+price_content_label.pack(anchor="w")
+##--------##Tkinter Rating##--------##
+rating_label = tk.Label(root, text="Rating: ", font='Helvetica 11 bold',pady=5).pack(anchor="w")
+rating_content_label = ttk.Label(root, text=productLists[productIndex].rating)
+rating_content_label.pack(anchor="w")
+##--------##Tkinter Buttons##--------##
+button_frame = Frame(root)
+button_frame.pack()
+previous_button = ttk.Button(button_frame,text="Anterior",command=previous)
+previous_button.pack(side=LEFT)
+next_button = ttk.Button(button_frame,text="Siguiente",command=next)
+next_button.pack(side=RIGHT)
+##--------##MainLoop##--------##
 root.mainloop()
